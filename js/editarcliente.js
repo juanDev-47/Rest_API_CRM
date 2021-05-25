91a564ae4764c37aa6318b7266eb3d9514a09fa1
juanDev-47 <juanp.arenas@udea.edu.co>
2021-05-24 23:04:23 -0500

    APP uploaded

---
+++ js/editarcliente.js
@@ -0,0 +1,64 @@
+import { obtenerCliente, editarCliente } from './API.js';
+import { mostrarAlerta, validar } from './funciones.js';
+
+
+(function() {
+
+    // campos del formulario
+    const nombreInput = document.querySelector('#nombre');
+    const emailInput = document.querySelector('#email');
+    const telefonoInput = document.querySelector('#telefono');
+    const empresaInput = document.querySelector('#empresa');
+    const idInput = document.querySelector('#id');
+
+    document.addEventListener('DOMContentLoaded', async () => {
+        const parametrosURL = new URLSearchParams(window.location.search);
+
+        const idCliente = parseInt( parametrosURL.get('id'));
+
+        const cliente = await obtenerCliente(idCliente);
+        mostrarCliente(cliente);
+
+        // submit al formulario
+        const formulario = document.querySelector('#formulario');
+        formulario.addEventListener('submit', validarCliente);
+    
+
+    });
+
+    function validarCliente(e) {
+        e.preventDefault();
+
+        const cliente = {
+            nombre: nombreInput.value,
+            email: emailInput.value,
+            telefono: telefonoInput.value,
+            empresa: empresaInput.value,
+            id: parseInt(idInput.value)
+        }
+
+
+        if( validar(cliente)) {
+            // mostrar mensaje
+            mostrarAlerta('Todos los campos son obligatorios')
+
+            return;
+        }
+
+        //Reescribir el objeto
+        editarCliente(cliente);
+
+    }
+
+    function mostrarCliente(cliente){
+        const { nombre, email, telefono, empresa, id } = cliente;
+
+        nombreInput.value = nombre;
+        empresaInput.value = empresa;
+        telefonoInput.value = telefono;
+        emailInput.value = email;
+        idInput.value = id;
+        
+
+    }
+})();
